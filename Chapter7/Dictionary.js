a2842ed3ab8187dbadd3546512c7e4de165c26c1
Mikhail Metrikin <metrikin@gmail.com>
2017-08-17 21:27:20 -0700

    Added dictionary example

---
+++ Chapter7/Dictionary.js
@@ -0,0 +1,51 @@
+function Dictionary() {
+  const dataStore = [];
+  count = 0;
+
+  function add(key, value) {
+    dataStore[key] = value;
+    count++;
+  }
+
+  function find(key) {
+    return dataStore[key];
+  }
+
+  function remove(key) {
+    delete dataStore[key];
+    --count;
+  }
+
+  function showAll() {
+    for (let key in dataStore) {
+      console.log(`Key is ${key} and value is ${dataStore[key]}\n`);
+    }
+  }
+
+  function getCount() {
+    return count;
+  }
+
+  function clear() {
+    for (let item in dataStore) {
+      delete dataStore[item];
+    }
+  }
+
+  return {
+    add,
+    find,
+    remove,
+    showAll,
+    getCount
+  };
+}
+
+var pbook = Dictionary();
+pbook.add('Mike', '123');
+pbook.add('David', '345');
+pbook.add('Cynthia', '456');
+console.log("David's extension: " + pbook.find('David'));
+pbook.remove('David');
+pbook.showAll();
+console.log(`Number if entries ${pbook.getCount()}`);
